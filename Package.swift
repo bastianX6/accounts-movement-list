de20b697026e103cb3f74285645e65637051c9ef
Bastian Veliz <bastianx6@gmail.com>
2020-11-22 16:40:53 -0300

    Revert Swift Package monorepo configuration

--- Package.swift
+++ Package.swift
@@ -14,29 +14,77 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "MovementList",
-            targets: ["MovementList"]
-        )
+            name: "MovementListCommon",
+            targets: ["MovementListCommon"]
+        ),
+        .library(
+            name: "MovementList_iOS",
+            targets: ["MovementList_iOS"]
+        ),
+        .library(
+            name: "MovementDetails_iOS",
+            targets: ["MovementDetails_iOS"]
+        ),
     ],
     dependencies: [
-        .package(path: "./MovementList_iOS"),
-        .package(path: "./MovementDetails_iOS"),
+        .package(name: "DataManagement",
+                 url: "https://github.com/bastianX6/accounts-data-management.git",
+                 .upToNextMinor(from: "1.0.1")),
+        .package(name: "AccountsUI",
+                 url: "https://github.com/bastianX6/accounts-ui.git",
+                 .upToNextMinor(from: "1.2.0")),
+        .package(name: "DependencyResolver",
+                 url: "https://github.com/bastianX6/accounts-dependency-resolver",
+                 .upToNextMinor(from: "1.0.0")),
+        .package(name: "NewMovement",
+                 url: "https://github.com/bastianX6/accounts-new-movement.git",
+                 .upToNextMinor(from: "1.1.1")),
     ],
     targets: [
+        // MovementListCommon
         .target(
-            name: "MovementList",
+            name: "MovementListCommon",
             dependencies: [
-                .product(
-                    name: "MovementList_iOS",
-                    package: "MovementList_iOS",
-                    condition: .when(platforms: [.iOS])
-                ),
-                .product(
-                    name: "MovementDetails_iOS",
-                    package: "MovementDetails_iOS",
-                    condition: .when(platforms: [.iOS])
-                )
-            ]
+                "DataManagement",
+                "AccountsUI",
+            ],
+            resources: [.process("Resources")]
+        ),
+        .testTarget(
+            name: "MovementListCommonTests",
+            dependencies: ["MovementListCommon"]
+        ),
+        // MovementList_iOS
+        .target(
+            name: "MovementList_iOS",
+            dependencies: [
+                "DataManagement",
+                "AccountsUI",
+                "MovementListCommon",
+                "MovementDetails_iOS",
+                "DependencyResolver",
+            ],
+            resources: [.process("Resources")]
+        ),
+        .testTarget(
+            name: "MovementList.iOS.Tests",
+            dependencies: ["MovementList_iOS"]
+        ),
+        // MovementDetails_iOS
+        .target(
+            name: "MovementDetails_iOS",
+            dependencies: [
+                "DataManagement",
+                "AccountsUI",
+                "MovementListCommon",
+                "DependencyResolver",
+                .product(name: "NewMovement_iOS", package: "NewMovement")
+            ],
+            resources: [.process("Resources")]
+        ),
+        .testTarget(
+            name: "MovementDetails.iOS.Tests",
+            dependencies: ["MovementDetails_iOS"]
         )
     ]
 )
